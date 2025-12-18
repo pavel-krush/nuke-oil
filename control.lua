f29a6fa6abb5601880a0c0cc167bef13fa5e10b7
Pavel Krush <neon@pushwoosh.com>
2025-12-18 17:51:49 +0700

    reindent

--- control.lua
+++ control.lua
@@ -2,33 +2,33 @@ local NUKE_RADIUS = 12 -- real radius is 35, but let's destroy patches in smalle
 local OIL_RESOURCE_NAME = "crude-oil"
 
 local function destroy_oil_patches(surface, position, radius)
-    local oil_patches = surface.find_entities_filtered{
-        area = {
-            {position.x - radius, position.y - radius},
-            {position.x + radius, position.y + radius}
-        },
-        type = "resource",
-        name = OIL_RESOURCE_NAME
-    }
+	local oil_patches = surface.find_entities_filtered{
+		area = {
+			{position.x - radius, position.y - radius},
+			{position.x + radius, position.y + radius}
+		},
+		type = "resource",
+		name = OIL_RESOURCE_NAME
+	}
 
-    for _, oil_patch in pairs(oil_patches) do
-    	local pos = oil_patch.position
-        oil_patch.destroy()
-        surface.create_entity{
-            name = "big-explosion",
-            position = pos
-        }
-    end
+	for _, oil_patch in pairs(oil_patches) do
+		local pos = oil_patch.position
+		oil_patch.destroy()
+		surface.create_entity{
+			name = "big-explosion",
+			position = pos
+		}
+	end
 end
 
 local function on_script_trigger(event)
-    if event.effect_id == "nuke-oil-destroy" then
-        local surface = game.surfaces[event.surface_index]
+	if event.effect_id == "nuke-oil-destroy" then
+		local surface = game.surfaces[event.surface_index]
 
-        if surface and event.source_position then
-            destroy_oil_patches(surface, event.source_position, NUKE_RADIUS)
-        end
-    end
+		if surface and event.source_position then
+			destroy_oil_patches(surface, event.source_position, NUKE_RADIUS)
+		end
+	end
 end
 
 script.on_event(defines.events.on_script_trigger_effect, on_script_trigger)
--- data-updates.lua
+++ data-updates.lua
@@ -1,35 +1,35 @@
 local insert = table.insert
 
 insert(data.raw.projectile["atomic-bomb-wave"].action,
-  {
-    type = "direct",
-    action_delivery =
-    {
-      type = "instant",
-      target_effects =
-      {
-        {
-          type = "script",
-          effect_id = "nuke-oil-destroy"
-        }
-      }
-    }
-  })
+	{
+		type = "direct",
+		action_delivery =
+		{
+			type = "instant",
+			target_effects =
+			{
+				{
+					type = "script",
+					effect_id = "nuke-oil-destroy"
+				}
+			}
+		}
+	})
 
 if data.raw.projectile["atomic-artillery-wave"] then
-  insert(data.raw.projectile["atomic-artillery-wave"].action,
-    {
-      type = "direct",
-      action_delivery =
-      {
-        type = "instant",
-        target_effects =
-        {
-          {
-            type = "script",
-            effect_id = "nuke-oil-destroy"
-          }
-        }
-      }
-    })
+	insert(data.raw.projectile["atomic-artillery-wave"].action,
+		{
+			type = "direct",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects =
+				{
+					{
+						type = "script",
+						effect_id = "nuke-oil-destroy"
+					}
+				}
+			}
+		})
 end
